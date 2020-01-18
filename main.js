8b83208ffb31fbc596ac54aeb099d595cea3e212
Yeluri <ay185058@ncr.com>
2020-01-18 11:02:36 +0530

    random quote

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-//let FlickrRequest=  "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=e507d89017626ef9a61e8a1dc8268d61&gallery_id=6065-72157617483228192&per_page=&format=json&nojsoncallback=1"
 let FlickrRequest="https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e507d89017626ef9a61e8a1dc8268d61&format=json&nojsoncallback=1"
+let QuoteRequest="https://api.quotable.io/random"
 var data;
+
+var Request=new XMLHttpRequest()
+Request.open('GET',QuoteRequest,true)
+
+Request.onload=function(){
+    quote=JSON.parse(Request.response)
+    document.getElementById('title').innerHTML =quote.content
+}
+Request.send()
+
+
 var request = new XMLHttpRequest()
 request.open('GET',FlickrRequest,true)
 
@@ -16,7 +27,7 @@ request.onload =function() {
     
     let FlickrUrl="https://farm"+farmId+".staticflickr.com/"+serverId+"/"+id+"_"+secret+"_b.jpg"
     //document.body.style.backgroundImage = "url("+Wallpapers[Math.floor(Math.random() * Wallpapers.length)]+")";
-    document.getElementById('title').innerHTML =''+title;
+    //document.getElementById('title').innerHTML =''+title;
     document.body.style.backgroundImage = "url("+FlickrUrl+")";
     document.body.style.height="100px"
     document.body.style.position="center"
